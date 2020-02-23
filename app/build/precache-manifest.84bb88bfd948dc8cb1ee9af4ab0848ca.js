40e5172d0d2b69eac18bf16e6fde13d9eb19b779
RyanMulka <Ryan.Mulka@target.com>
2020-02-22 20:46:50 -0600

    edits for production

---
+++ app/build/precache-manifest.84bb88bfd948dc8cb1ee9af4ab0848ca.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbfda92ad918055bc4bf30a4b0af8a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd8264dd2f976f98e8ee",
+    "url": "/static/css/main.b3e41f3e.chunk.css"
+  },
+  {
+    "revision": "b42822f9d5a32f36ef79",
+    "url": "/static/js/2.a5a785c8.chunk.js"
+  },
+  {
+    "revision": "2549765df2eb04fa59a1f28130514f65",
+    "url": "/static/js/2.a5a785c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8264dd2f976f98e8ee",
+    "url": "/static/js/main.1d2d3a8a.chunk.js"
+  },
+  {
+    "revision": "ef75a6ed8a6174a01bc6",
+    "url": "/static/js/runtime-main.225e90d1.js"
+  },
+  {
+    "revision": "7f95e80ad960112dfc1377efb3a91be7",
+    "url": "/static/media/citizens.7f95e80a.jpg"
+  },
+  {
+    "revision": "b43b69a0b278745c04737f8858737e35",
+    "url": "/static/media/mafia-background.b43b69a0.jpg"
+  },
+  {
+    "revision": "d5939b3dc96d1b720f22eb2243c7e172",
+    "url": "/static/media/mafia.d5939b3d.jpg"
+  }
+]);
